1e2e41570f14324795ed2081094462cf776b49a7
Eric Stratton <strattonericj@gmail.com>
2020-11-13 08:37:59 -0800

    add first test, bad fail only at this time

---
+++ __tests__/mercury.test.js
@@ -0,0 +1,9 @@
+import MercuryAge from '../src/js/mercury.js';
+// import { TestScheduler } from 'jest';
+
+describe('MercuryAge', () => {
+
+  test('should correctly calculate age on Mercury based on rotations around sun relative to Earth')
+  let age = new MercuryAge()
+  expect(age).toEqual() 
+});
